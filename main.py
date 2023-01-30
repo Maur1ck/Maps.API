71b2b3deab556b869a31843a861384dd7374224f
Misha <user@yandex.ru>
2023-01-30 19:16:03 +0300

    2 тз pu&pd

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
 import os
 import sys
 import requests
+from io import BytesIO
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QApplication, QLineEdit, QLabel
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.getImage()
         self.initUI()
-
-    def getImage(self):
-        delta = "0.005"
-        map_params = {
-            "ll": "37.530887,55.70311",
-            "spn": ",".join([delta, delta]),
-            "l": "map"
-        }
-        map_api_server = "http://static-maps.yandex.ru/1.x/"
-        response = requests.get(map_api_server, params=map_params)
-
-        if not response:
-            print("Ошибка выполнения запроса:")
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
-
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
 
     def initUI(self):
         self.setGeometry(300, 300, 600, 500)
@@ -43,11 +25,46 @@ class MainWindow(QWidget):
         self.name_input.setText("Для next тз")
         self.name_input.setDisabled(True)
 
-        self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 50)
         self.image.resize(600, 450)
+
+        self.delta = "10"
+        self.getImage()
+
+    def getImage(self):
+        print(self.delta)
+        map_params = {
+            "ll": "37.530887,55.70311",
+            "z": self.delta,
+            "l": "map"
+        }
+        map_api_server = "http://static-maps.yandex.ru/1.x/"
+        response = requests.get(map_api_server, params=map_params)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+        self.draw_img()
+
+    def draw_img(self):
+        print(1)
+        self.pixmap = QPixmap(self.map_file)
         self.image.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if int(self.delta) < 17:
+                self.delta = str(int(self.delta) + 1)
+        if event.key() == Qt.Key_PageDown:
+            if int(self.delta) > 0:
+                self.delta = str(int(self.delta) - 1)
+        self.getImage()
 
     def closeEvent(self, event):
         os.remove(self.map_file)
